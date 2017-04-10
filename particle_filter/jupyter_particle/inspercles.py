5ea32b60528c67fd81e9a6e78a1a561a4374caf0
Robotica Computacional <Otofuji@gituhub.com>
2017-04-10 15:43:43 -0300

    quase

--- particle_filter/jupyter_particle/inspercles.py
+++ particle_filter/jupyter_particle/inspercles.py
@@ -258,13 +258,13 @@ def nb_simulate_lidar(robot_pose, angles, img):
             result_img[int(y), int(x)] = 0 # Marcamos o raio na imagem y,x porque numpy e' linha, coluna
             if nb_outside_image(int(x), int(y), img):
                 # A imagem acabou, nao achamos nada
-                lidar_results[ang] = -1   
+                lidar_results[angulo] = -1   
                 print("Outside at ",x ,"  ",y, "  for angle ", ang)
                 break
             dist = nb_found_obstacle(int(y), int(x), y0, x0, img)
             if dist > -1:   
                 # Achamos alguma coisa
-                lidar_results[ang] = dist 
+                lidar_results[angulo] = dist 
                 #print("Hit for ",x,  "  ",y, "  for angle ", ang)                
                 break
             # Keep going if none of the "ifs" has been triggered
